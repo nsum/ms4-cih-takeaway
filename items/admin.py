515dea033d3aaa15da98a09919cdeb78d30b1e26
nsum <your.marketer1303@gmail.com>
2021-04-19 01:19:25 +0000

    Customize items & categories admin

--- items/admin.py
+++ items/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 from .models import Item, Category
 
 
-admin.site.register(Item)
-admin.site.register(Category)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'allergens',
+        'price',
+    )
+
+    ordering = ('category',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category, CategoryAdmin)
