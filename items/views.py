76c00c3e37975db2173722d2d48f5959ca73b849
nsum <your.marketer1303@gmail.com>
2021-04-28 04:07:15 +0000

    Add search bar to nav and search functionality

--- items/views.py
+++ items/views.py
@@ -1,14 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from .models import Item
+from django.contrib import messages
+from django.db.models import Q
 
 # Create your views here.
 
 
 def all_items(request):
     items = Item.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('items'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            items = items.filter(queries)
 
     context = {
         'items': items,
+        'search_term': query,
     }
 
     return render(request, 'items/items.html', context)
