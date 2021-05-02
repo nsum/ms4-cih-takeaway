9b7e56a3ad93fe0164225c4819923f5471c4655c
nsum <your.marketer1303@gmail.com>
2021-05-02 01:24:29 +0000

    Add product sorting and change rating system to one decimal

--- items/views.py
+++ items/views.py
@@ -10,8 +10,22 @@ def all_items(request):
     items = Item.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                items = items.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            items = items.order_by(sortkey)
 
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
@@ -26,11 +40,14 @@ def all_items(request):
 
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             items = items.filter(queries)
+    
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'items': items,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'items/items.html', context)
