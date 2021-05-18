70c40aa2dd060dd191e2c191ae05cf4139a01839
nsum <your.marketer1303@gmail.com>
2021-05-18 04:52:01 +0000

    Add admin, forms & signals to checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -12,12 +12,10 @@ class Order(models.Model):
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     order_discount = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
@@ -32,13 +30,14 @@ class Order(models.Model):
         super().save(*args, **kwargs)
 
     def update_total(self):
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total'))['lineitem_total__sum']
         if self.order_total < settings.DISCOUNT_THRESHOLD:
-            self.discount = 0
+            self.order_discount = 0
         else:
-            self.discount = self.order_total * Decimal(
+            self.order_discount = self.order_total * Decimal(
                 settings.DISCOUNT_PERCENTAGE / 100)
-        self.grand_total = self.order_total - self.discount
+        self.grand_total = self.order_total - self.order_discount
         self.save()
 
     def __str__(self):
