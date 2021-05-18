4b7ca7dde54b2b45efce56e9b4f33e18a0611a74
nsum <your.marketer1303@gmail.com>
2021-05-18 04:15:25 +0000

    Add checkout app & models, remove item image property

--- items/models.py
+++ items/models.py
@@ -23,8 +23,6 @@ class Item(models.Model):
     allergens = models.CharField(max_length=254, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
